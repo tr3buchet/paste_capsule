6bfe44948f657f8f3d6eb76dda6f8f4d4091f5c8
Trey Morris <treyemorris@gmail.com>
2014-07-03 04:44:49 -0500

    test

--- paste_capsule/paste_capsule.py
+++ paste_capsule/paste_capsule.py
@@ -89,7 +89,7 @@ def paste_create():
 
 def paste_show(paste_uuid):
     p = r.get('paste:%s' % paste_uuid)
-    return flask.render_template('paste_show', paste=p)
+    return flask.render_template('paste_show.html', paste=p)
 #    return flask.Response(p, mimetype='text/plain')
 
 
